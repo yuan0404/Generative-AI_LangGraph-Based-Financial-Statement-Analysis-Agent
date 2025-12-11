da9c4576d3e8f29b662c172c1bbaf8c51edd3e2e
yuan0404 <163089490+yuan0404@users.noreply.github.com>
2025-12-11 23:24:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from dotenv import load_dotenv
 from datetime import datetime
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from pipeline import init_graph, init_state
 
 
 app = FastAPI(title="Financial Statement Analysis Agent API")
+
+origins = [
+    "http://localhost",
+    "http://localhost:5500",
+    "https://yuan0404.github.io"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Input(BaseModel):
@@ -29,3 +44,4 @@ def analyze(input: Input):
     memory.append({"user": user_input, "assistant": answer})
 
     return result
+
